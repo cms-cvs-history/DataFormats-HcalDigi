85e9b90d58e8f9f4e97e0b321652e8cdd1f61744
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2011-01-21 20:26:14 +0000

    First commit for slhc_hcal_branch_3_6_X

--- src/classes.h
+++ src/classes.h
@@ -9,28 +9,34 @@
 #include "DataFormats/HcalDigi/interface/HcalUnpackerReport.h"
 #include "DataFormats/HcalDigi/interface/HcalLaserDigi.h"
 #include "DataFormats/HcalDigi/interface/HcalTTPDigi.h"
+#include "DataFormats/HcalDigi/interface/HcalUpgradeDataFrame.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
   struct dictionary {
     std::vector<HcalQIESample> vQIE_;
     std::vector<HcalTriggerPrimitiveSample> vTPS_;
+    std::vector<HcalUpgradeTriggerPrimitiveSample> vUTPS_;
     
     edm::SortedCollection<HBHEDataFrame> vHBHE_;
     edm::SortedCollection<HODataFrame> vHO_;
     edm::SortedCollection<HFDataFrame> vHF_;
     edm::SortedCollection<HcalCalibDataFrame> vHC_;
     edm::SortedCollection<HcalTriggerPrimitiveDigi> vHTP_;
+    edm::SortedCollection<HcalUpgradeTriggerPrimitiveDigi> vHUTP_;
     edm::SortedCollection<HcalHistogramDigi> vHH_;
     edm::SortedCollection<HcalTTPDigi> vTTP_;
+    edm::SortedCollection<HcalUpgradeDataFrame> vU_;
 
     HBHEDigiCollection theHBHE_;
     HODigiCollection theHO_;
     HFDigiCollection theHF_;
     HcalCalibDigiCollection theHC_;
     HcalTrigPrimDigiCollection theHTP_;
+    HcalUpgradeTrigPrimDigiCollection theHUTP_;
     HcalHistogramDigiCollection theHH_;
     ZDCDigiCollection theZDC_;
+    HcalUpgradeDigiCollection theUG_;
     CastorDigiCollection theCastor_;
     HOTrigPrimDigiCollection theHOTP_;
     HcalTTPDigiCollection theTTP_;
@@ -40,22 +46,26 @@ namespace {
     edm::Wrapper<edm::SortedCollection<HFDataFrame> > anotherHF_;
     edm::Wrapper<edm::SortedCollection<HcalCalibDataFrame> > anotherHC_;
     edm::Wrapper<edm::SortedCollection<HcalTriggerPrimitiveDigi> > anotherHTP_;
+    edm::Wrapper<edm::SortedCollection<HcalUpgradeTriggerPrimitiveDigi> > anotherHUTP_;
     edm::Wrapper<edm::SortedCollection<HcalHistogramDigi> > anotherHH_;
     edm::Wrapper<edm::SortedCollection<ZDCDataFrame> > anotherZDC_;
     edm::Wrapper<edm::SortedCollection<CastorDataFrame> > anotherCastor_;
     edm::Wrapper<edm::SortedCollection<HOTriggerPrimitiveDigi> > anotherHOTP_;
     edm::Wrapper<edm::SortedCollection<HcalTTPDigi> > anotherTTP_;
+    edm::Wrapper<edm::SortedCollection<HcalUpgradeDataFrame> > anotherUG_;
 
     edm::Wrapper<HBHEDigiCollection> theHBHEw_;
     edm::Wrapper<HODigiCollection> theHOw_;
     edm::Wrapper<HFDigiCollection> theHFw_;
     edm::Wrapper<HcalCalibDigiCollection> theHCw_;
     edm::Wrapper<HcalTrigPrimDigiCollection> theHTPw_; 
+    edm::Wrapper<HcalUpgradeTrigPrimDigiCollection> theHUTPw_; 
     edm::Wrapper<HOTrigPrimDigiCollection> theHOTPw_; 
     edm::Wrapper<HcalHistogramDigiCollection> theHHw_; 
     edm::Wrapper<HcalUnpackerReport> theReport_;
     edm::Wrapper<HcalLaserDigi> theLaserw_;
     edm::Wrapper<HcalTTPDigiCollection> theTTPw_;
+    edm::Wrapper<HcalUpgradeDigiCollection> theUGw_;
   };
 }
 
